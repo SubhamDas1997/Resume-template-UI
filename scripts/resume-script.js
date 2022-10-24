d6febec12c9aa116ed8ef070e88fbe7f0a6e518c
SubhamDas1997 <das.subham31@yahoo.com>
2022-10-24 14:16:07 +0530

    Search functionality optimised

--- scripts/resume-script.js
+++ scripts/resume-script.js
@@ -26,6 +26,8 @@ const previousBtnEle = document.getElementById('previous');
 const nextBtnEle = document.getElementById('next');
 const currentPageEle = document.getElementById('current-page');
 const totalPagesEle = document.getElementById('total-pages');
+const resumeheadingEle = document.querySelector('.resume-heading');
+const containerEle = document.querySelector('.container');
 
 function fetchResumes() {
     fetch("./data/Data.json")
@@ -38,34 +40,51 @@ fetchResumes();
 function filterResumes(resumesList) {
     let tempArraywithDup = resumesList.resume.map(element => element.basics.AppliedFor);
     let jobsArray = [...new Set(tempArraywithDup)];
-    let filteredArray = [];
-    
-    showResume(resumesList);
+
+    showResume(resumesList.resume);
 
     searchEle.addEventListener("input", function() {
-        let value = this.value.toLowerCase();
-        
+        let filteredArray = [];
+        let arrToSend = [];
+        let userInp = this.value.toLowerCase();
+
         for(let i = 0; i < jobsArray.length; i++) {
-            if(jobsArray[i].toLowerCase().includes(value)) {
+            let isInputPresent = jobsArray[i].toLowerCase().includes(userInp);
+            // console.log(isInputPresent)
+
+            if(isInputPresent) {
+                // console.log(jobsArray[i])
                 filteredArray.resume = resumesList.resume.filter( element => {
-                    return element.basics.AppliedFor == jobsArray[i];
+                    return element.basics.AppliedFor == jobsArray[i]
                 });
+                // console.log(filteredArray)
+                arrToSend = arrToSend.concat(filteredArray.resume);
             }
         }
+        // console.log('Final array =')
+        // console.log(arrToSend)
+
         skillEle.innerHTML = '';
         hobbyEle.innerHTML = '';
         achievementEle.innerHTML = '';
-        showResume(filteredArray);
+        showResume(arrToSend);
     });
 }
 
-function showResume(resumeArray) {  
+function showResume(resumeArray) {
+    console.log(resumeArray)
     let firstResume = 0;
-    let lastResume = resumeArray.resume.length - 1;
+    let lastResume = resumeArray.length - 1;
+    // console.log(`total = ${lastResume}`)
+    
     let currentResume = 0;
+    // console.log(`current = ${currentResume}`)
+
     
     previousBtnEle.style.visibility = "hidden";
-    if(firstResume == lastResume) nextBtnEle.style.visibility = "hidden";
+    if(firstResume == lastResume) {
+        nextBtnEle.style.visibility = "hidden";
+    }
     
     currentPageEle.innerHTML = currentResume + 1;
     totalPagesEle.innerHTML = lastResume + 1;
@@ -74,6 +93,8 @@ function showResume(resumeArray) {
 
     nextBtnEle.addEventListener("click", function() {
         currentResume++;
+        // console.log(`current = ${currentResume}`)
+
         skillEle.innerHTML = '';
         hobbyEle.innerHTML = '';
         achievementEle.innerHTML = '';
@@ -86,6 +107,8 @@ function showResume(resumeArray) {
 
     previousBtnEle.addEventListener("click", function() {
         currentResume--;
+        console.log(`current = ${currentResume}`)
+
         skillEle.innerHTML = '';
         hobbyEle.innerHTML = '';
         achievementEle.innerHTML = '';
@@ -98,57 +121,57 @@ function showResume(resumeArray) {
 }
 
 function updatePage(resumesArray, resumeNo) {
-    appNameELe.innerHTML = resumesArray.resume[resumeNo].basics.name;
+    appNameELe.innerHTML = resumesArray[resumeNo].basics.name;
 
-    appForEle.innerHTML = `Applied For: ${resumesArray.resume[resumeNo].basics.AppliedFor}`;
+    appForEle.innerHTML = `Applied For: ${resumesArray[resumeNo].basics.AppliedFor}`;
 
-    // imgELe.innerHTML = `<img src="${resumesArray.resume[resumeNo].basics.image}" alt="Loading...">`
+    // imgELe.innerHTML = `<img src="${resumesArray[resumeNo].basics.image}" alt="Loading...">`
 
-    phoneEle.innerHTML = resumesArray.resume[resumeNo].basics.phone;
+    phoneEle.innerHTML = resumesArray[resumeNo].basics.phone;
 
-    emailEle.innerHTML = resumesArray.resume[resumeNo].basics.email;
+    emailEle.innerHTML = resumesArray[resumeNo].basics.email;
 
-    socialEle.href = resumesArray.resume[resumeNo].basics.profiles.url;
+    socialEle.href = resumesArray[resumeNo].basics.profiles.url;
 
-    socialEle.innerHTML = resumesArray.resume[resumeNo].basics.profiles.network;
+    socialEle.innerHTML = resumesArray[resumeNo].basics.profiles.network;
 
-    resumesArray.resume[resumeNo].skills.keywords.forEach(element => {
+    resumesArray[resumeNo].skills.keywords.forEach(element => {
         skillEle.innerHTML += `<li class="content">${element}</li>`;
     });
 
-    resumesArray.resume[resumeNo].interests.hobbies.forEach(element => {
+    resumesArray[resumeNo].interests.hobbies.forEach(element => {
         hobbyEle.innerHTML += `<li class="content">${element}</li>`;
     });
 
-    companyNameEle.innerHTML = `<strong>Company Name:</strong> ${resumesArray.resume[resumeNo].work['Company Name']}`;
+    companyNameEle.innerHTML = `<strong>Company Name:</strong> ${resumesArray[resumeNo].work['Company Name']}`;
 
-    companyPositionEle.innerHTML = `<strong>Position:</strong> ${resumesArray.resume[resumeNo].work['Position']}`;
+    companyPositionEle.innerHTML = `<strong>Position:</strong> ${resumesArray[resumeNo].work['Position']}`;
 
-    companyStartDateEle.innerHTML = `<strong>Start Date:</strong> ${resumesArray.resume[resumeNo].work['Start Date']}`;
+    companyStartDateEle.innerHTML = `<strong>Start Date:</strong> ${resumesArray[resumeNo].work['Start Date']}`;
 
-    companyEndDateEle.innerHTML = `<strong>End Date:</strong> ${resumesArray.resume[resumeNo].work['End Date']}`;
+    companyEndDateEle.innerHTML = `<strong>End Date:</strong> ${resumesArray[resumeNo].work['End Date']}`;
 
-    companySummaryEle.innerHTML = `<strong>Summary:</strong> ${resumesArray.resume[resumeNo].work['Summary']}`;
+    companySummaryEle.innerHTML = `<strong>Summary:</strong> ${resumesArray[resumeNo].work['Summary']}`;
 
-    projectEle.innerHTML = `<strong>${resumesArray.resume[resumeNo].projects.name}:</strong> ${resumesArray.resume[resumeNo].projects.description}`;
+    projectEle.innerHTML = `<strong>${resumesArray[resumeNo].projects.name}:</strong> ${resumesArray[resumeNo].projects.description}`;
 
-    ugEle.innerHTML = `<strong>UG:</strong> ${resumesArray.resume[resumeNo].education.UG.institute}, ${resumesArray.resume[resumeNo].education.UG.course}, ${resumesArray.resume[resumeNo].education.UG['Start Date']}, ${resumesArray.resume[resumeNo].education.UG['End Date']}, ${resumesArray.resume[resumeNo].education.UG.cgpa}`;
+    ugEle.innerHTML = `<strong>UG:</strong> ${resumesArray[resumeNo].education.UG.institute}, ${resumesArray[resumeNo].education.UG.course}, ${resumesArray[resumeNo].education.UG['Start Date']}, ${resumesArray[resumeNo].education.UG['End Date']}, ${resumesArray[resumeNo].education.UG.cgpa}`;
 
-    puEle.innerHTML = `<strong>PU:</strong> ${resumesArray.resume[resumeNo].education['Senior Secondary'].institute}, ${resumesArray.resume[resumeNo].education['Senior Secondary'].cgpa}`;
+    puEle.innerHTML = `<strong>PU:</strong> ${resumesArray[resumeNo].education['Senior Secondary'].institute}, ${resumesArray[resumeNo].education['Senior Secondary'].cgpa}`;
 
-    highSchoolEle.innerHTML = `<strong>PU:</strong> ${resumesArray.resume[resumeNo].education['High School'].institute}, ${resumesArray.resume[resumeNo].education['High School'].cgpa}`;
+    highSchoolEle.innerHTML = `<strong>PU:</strong> ${resumesArray[resumeNo].education['High School'].institute}, ${resumesArray[resumeNo].education['High School'].cgpa}`;
 
-    internNameEle.innerHTML = `<strong>Company Name:</strong> ${resumesArray.resume[resumeNo].Internship['Company Name']}`;
+    internNameEle.innerHTML = `<strong>Company Name:</strong> ${resumesArray[resumeNo].Internship['Company Name']}`;
 
-    internPositionEle.innerHTML = `<strong>Position:</strong> ${resumesArray.resume[resumeNo].Internship['Position']}`;
+    internPositionEle.innerHTML = `<strong>Position:</strong> ${resumesArray[resumeNo].Internship['Position']}`;
 
-    internStartEle.innerHTML = `<strong>Start Date:</strong> ${resumesArray.resume[resumeNo].Internship['Start Date']}`;
+    internStartEle.innerHTML = `<strong>Start Date:</strong> ${resumesArray[resumeNo].Internship['Start Date']}`;
 
-    internEndEle.innerHTML = `<strong>End Date:</strong> ${resumesArray.resume[resumeNo].Internship['End Date']}`;
+    internEndEle.innerHTML = `<strong>End Date:</strong> ${resumesArray[resumeNo].Internship['End Date']}`;
 
-    internSummaryEle.innerHTML = `<strong>Summary:</strong> ${resumesArray.resume[resumeNo].Internship['Summary']}`;
+    internSummaryEle.innerHTML = `<strong>Summary:</strong> ${resumesArray[resumeNo].Internship['Summary']}`;
 
-    resumesArray.resume[resumeNo].achievements.Summary.forEach(element => {
+    resumesArray[resumeNo].achievements.Summary.forEach(element => {
         achievementEle.innerHTML += `<li>${element}</li>`;
     });
 }
